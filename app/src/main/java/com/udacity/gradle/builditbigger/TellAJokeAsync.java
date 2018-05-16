f799cd72010e7176e7d30fbd06eaa59e9b8dd640
WEBARTIST\User <hennasingh04@gmail.com>
2018-05-16 17:55:18 +0100

    added Joke Bean, jokedata and edited async task to return arraylist of strings and updated ip address

--- app/src/main/java/com/udacity/gradle/builditbigger/TellAJokeAsync.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/TellAJokeAsync.java
@@ -6,43 +6,53 @@ import android.util.Log;
 import com.artist.web.jokestore.JokeTeller;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 /**
  * Created by User on 15-May-18.
  */
 
-public class TellAJoke extends AsyncTask<String, Void, JokeTeller> {
+public class TellAJokeAsync extends AsyncTask<String, Void, ArrayList<ArrayList<String>>> {
 
     private OnEventListener<JokeTeller> mCallback;
     private WeakReference<MainActivity> appReference;
     public Exception mException;
     private static MyApi sMyApiService = null;
-    private static final String TAG = TellAJoke.class.getSimpleName();
+    private static final String TAG = TellAJokeAsync.class.getSimpleName();
 
 
-    public TellAJoke(OnEventListener<JokeTeller> callback, MainActivity context) {
+
+    public TellAJokeAsync(OnEventListener<JokeTeller> callback, MainActivity context) {
         mCallback = callback;
         appReference = new WeakReference<>(context);
     }
 
     @Override
-    protected JokeTeller doInBackground(String... strings) {
+    protected ArrayList<ArrayList<String>> doInBackground(String... strings) {
 
         if(sMyApiService==null){
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(),null)
-                    .setRootUrl("http://127.0.0.1:8080/_ah/api/");
+                    .setRootUrl("http://192.168.0.66:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                @Override
+                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+                        throws IOException {
+                    abstractGoogleClientRequest.setDisableGZipContent(true);
+                }
+            });
+
             sMyApiService = builder.build();
         }
-        String question = strings[0];
-        String answer = strings[1];
 
         try{
-            return sMyApiService.tellJoke(question, answer);
+            return sMyApiService.tellJoke().execute().getJokeList();
         }catch(IOException e){
             Log.e(TAG, "got an exception " + e.getMessage());
         }
