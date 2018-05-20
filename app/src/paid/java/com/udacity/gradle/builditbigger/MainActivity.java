19c92b623a0de6220e8d0f3e0b514b3edecd7b15
WEBARTIST\User <hennasingh04@gmail.com>
2018-05-20 03:44:42 +0100

    added layout resource file for paid flavor but connection times out needs fixing

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.artist.web.jokewizard.JokeActivity;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
